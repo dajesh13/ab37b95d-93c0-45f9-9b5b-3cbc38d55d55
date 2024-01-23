40f911083cc338f10e46c81df2f4a0adbecb9965
dajesh13 <ajesh.deepa13@gmail.com>
2024-01-24 01:37:05 +1100

    first commit

---
+++ CodingTestApp/FindSeq.cs
@@ -0,0 +1,49 @@
+﻿namespace CodingTestApp;
+
+public static class FindSeq
+{
+    public static string FindLongestSequence(string inputString)
+    {
+        string outputArray = "";
+        if (inputString != null)
+        {
+            var iparray = inputString.Split(' ');
+            int[] array = Array.ConvertAll(iparray, int.Parse);
+            outputArray = FindLongestIncSequence(array);
+        }
+        Console.Write(outputArray);
+        return outputArray;
+    }
+    private static string FindLongestIncSequence(int[] array)
+    {
+        List<int> list = new List<int>();
+        List<int> longestList = new List<int>();
+        int highestCount = 1;
+        for (int i = 0; i < array.Length; i++)
+        {
+            list.Add(array[i]);
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[i] < array[j])
+                {
+                    list.Add(array[j]);
+                    i++;
+                }
+                else
+                {
+                    break;
+                }
+                i = j;
+            }
+            // Compare with in previous lists  
+            if (highestCount < list.Count)
+            {
+                highestCount = list.Count;
+                longestList = new List<int>(list);
+            }
+            list.Clear();
+
+        }
+        return string.Join(" ", longestList); ;
+    }
+}
---
+++ CodingTestApp/FindSeqController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace CodingTestApp;
+
+[ApiController]
+[Route("api/FindSeqController")]
+public class FindSeqController : ControllerBase
+{
+   [HttpGet]
+    public string GetString(string inputString)
+    {
+        string output = "";
+        if(inputString == null)
+        {
+            NotFound();
+        }
+        else
+       output = FindSeq.FindLongestSequence(inputString);
+       return output;
+    }
+
+}
